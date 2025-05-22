3611598ae17e33594391d8451e4c8f1369445080
Medzy <Medhanshpatel18@gmail.com>
2025-05-22 23:12:41 +0530

    Add useState import to Services component

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import './Services.css';
 import serviceIcon from '/src/assets/serviceIcon.png';
